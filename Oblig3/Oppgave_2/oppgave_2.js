60d89e53960409a4ad8cbe5274422043b3c0dcf2
taki1611 <tarikj@hiof.no>
2021-09-08 20:12:26 +0200

    Initial commit

--- Oblig3/Oppgave_2/oppgave_2.js
+++ Oblig3/Oppgave_2/oppgave_2.js
@@ -2,7 +2,10 @@
 //import { users } from './util';
 
 // TODO: Hent HTML #id med getElementById
-document.getElementById("");
+const searchInput = '';
+const filterInput = '';
+const filterButton = '';
+const userUI = '';
 // TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
